a7165e9fc87e5183c0c3ec27f00bde98c99d3896
Gary Lizard <66739345+GaryLizard@users.noreply.github.com>
2025-12-10 16:02:19 -0600

    Update PackagePurge.sh to include more packages
    Added additional packages to be purged from the system.

--- PackagePurge.sh
+++ PackagePurge.sh
@@ -8,7 +8,30 @@ PACKAGES_TO_PURGE=(
     cowpatty hashcat hping3 armitage beef-xss sherlock gobuster bloodhound ettercap
     bettercap scapy autopsy metasploit-framework mimikatz sqlninja snort lynis dnschef 	
     spiderfoot nessus ophcrack-cli netcat ettercap-common ettercap-graphicsal ettercap-text-only
-    zenmap kismet dsniff fierce yersinia macchanger medusa cewl wfuzz enum4linux smbclient nbtclient snmpwalk snmp cupp cupp3 4g8
+    zenmap kismet dsniff fierce yersinia macchanger medusa cewl wfuzz enum4linux smbclient nbtclient 
+    snmpwalk snmp cupp cupp3 4g8 john-the-ripper hunt nbtscan airgeddon asleap automater bind9-host
+    bluelog blueranger bluesnarfer braa brutespray bully cge cisco-auditing-tool cisco-global-exploiter
+    cisco-ocs cisco-torch cms-explorer crunch cryptcat cutter cymothoa davtest dbd dcflddd dc3dd ddrescue
+    dex2jar dff dhcpig dmitry dnmap dns2tcp dnsenum dnsmap dnsrecon doona dos2unix dotdotpwn driftnet
+    dumpzilla eapmd5pass edb-debugger enumiax etherape exploitdb eyewitness faraday fern-wifi-cracker 
+    firewalk fragroute fragrouter freeradius freeradius-utils funkload galleta ghost-phisher giskismet
+    golismero goofile gqrx-sdr grabber guymager hackrf hamster-sidejack hashid hexinject hexorbase 
+    hotpatch httptunnel iaxflood inetsim inosuke inviteflood iodine irpas irssi johnny joomscan 
+    jsql keepnote killerbee laudanum lbd libfindrtp linphone linux-exploit-suggester lldpd 
+    magicrescue magictree man-in-the-middle-framework maskgen maskprocessor masscan mc
+    md5deep mdbtools mdk3 mfcuk mfoc mfterm miranda mitmproxy mitmf mona mongodb mongo-tools 
+    mosh msfpc msfpc-ng multimac-ng nasm ncrack ndiff nemesis netsed netsniff-ng netwag nipper-ng 
+    oclgausscrack oclhashcat ohrwurm onesixtyone openvas openvas-cli openvas-manager openvas-scanner
+    oscanner padbuster paros pasco patator pdf-parser pdfid pdfresurrect peepdf pev phrasendrescher pipal 
+    pixiewps plecost polenum policygen-test powersploit protos-sip proxychains proxytunnel pwnat qsslcaudit 
+    radare2 radamsa rainbowcrack rarcrack rcracki-mt reaver rebind recoverjpeg redsocks regripper responder rfdump 
+    ridenum rsmangler rtpbreak rtpflood rtpinsertsound rtpmixsound sbd scraper scrounge-ntfs seatbelt sendemail set 
+    sfuzz sidguesser siege siparmyknife sipcrack skipfish sleuthkit slowhttptest smali smbmap sniffjoke socat sparta 
+    spectools spikeproxy sploitctl sqldict sqlitebrowser sslcaudit sslh sslscan sslsniff sslsplit sslstrip sslyze 
+    statsprocessor sucrack swaks t50 tcpreplay tcpxtract teardrop testdisk thc-ipv6 thc-pptp-bruter thc-ssl-dos 
+    theharvester tnscmd10g truecrack twofi u3-pwn ubertooth udptunnel unhide unhide.rb uniscan unix-privesc-check 
+    vidalia volatility w3af wafw00f wash webshells webspa weevely wifi-honey wifiphisher wifitap windows-binaries 
+    wordlists xplico xspy xsser zaproxy zaproxy-common zaproxy-core zaproxy-proxy zaproxy-scripts zaproxy-ui
 )
 
 LOGFILE="/var/log/package_purge_$(date +%Y%m%d_%H%M%S).log"
@@ -121,3 +144,22 @@ sudo snap remove --purge snmp
 sudo snap remove --purge cupp 
 sudo snap remove --purge cupp3 
 sudo snap remove --purge 4g8
+sudo snap remove --purge john-the-ripper
+sudo snap remove --purge hunt
+sudo snap remove --purge nbtscan
+sudo snap remove --purge zaproxy
+sudo snap remove --purge set
+sudo snap remove --purge sqlitebrowser
+sudo snap remove --purge whatweb
+sudo snap remove --purge volatility
+sudo snap remove --purge autopsy
+sudo snap remove --purge testdisk
+sudo snap remove --purge foremost
+sudo snap remove --purge binwalk
+sudo snap remove --purge radare2
+sudo snap remove --purge ghidra
+sudo snap remove --purge cutter
+sudo snap remove --purge edb-debugger
+sudo snap remove --purge ollydbg
+sudo snap remove --purge x64dbg
+sudo snap remove --purge wpscan
