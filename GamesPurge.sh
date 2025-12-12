c244d1baa1c1ae831ab60bcf0fb325f325e2e5b2
Gary Lizard <66739345+GaryLizard@users.noreply.github.com>
2025-12-11 22:27:43 -0600

    Refactor GamesPurge.sh to remove nested bash command

--- GamesPurge.sh
+++ GamesPurge.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-bash -c "
-sudo apt purge -y \
-  *game* \
-  minetest* \
-  supertux* \
-  0ad* \
-  frozen-bubble \
-  gnome-games \
-  aisleriot \
-  gnome-mahjongg \
-  gnome-mines \
-  gnome-sudoku \
-  endless-sky \
-  zangband \
-  angband \
-  nethack* \
-  2>/dev/null
-  
-  sudo apt autoremove -y
-"
+
+
+sudo apt purge *game* 
+sudo apt purge minetest* 
+sudo apt purge supertux* 
+sudo apt purge 0ad* 
+sudo apt purge frozen-bubble 
+sudo apt purge gnome-games 
+sudo apt purge aisleriot 
+sudo apt purge gnome-mahjongg 
+sudo apt purge gnome-mines 
+sudo apt purge gnome-sudoku 
+sudo apt purge endless-sky 
+sudo apt purge zangband 
+sudo apt purge angband 
+sudo apt purge nethack* 
+sudo snap remove --purge duckmarines
+sudo snap remove --purge obs-studio
+
+sudo apt autoremove -y
